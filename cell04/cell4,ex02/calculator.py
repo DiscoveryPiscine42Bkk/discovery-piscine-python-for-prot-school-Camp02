28daadc3439bd8206a204801bdf5b1416c8b270f
wchueato <40608@prot.ac.th>
2025-03-11 10:27:05 +0700

    ex02

---
+++ cell04/cell4,ex02/calculator.py
@@ -0,0 +1,13 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+ผลบวก = num1 + num2
+ผลลบ = num1 - num2
+ผลคูณ = num1 * num2
+ผลหาร = num1 / num2
+
+print(ผลบวก)
+print(ผลลบ)
+print(ผลคูณ)
+print(ผลหาร)
+input()
